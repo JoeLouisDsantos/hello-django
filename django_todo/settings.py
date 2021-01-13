ea11e6b4f1cdc93f0e37d953c935a7e352af748d
Joe-Louis D'Santos <joelouis14@hotmail.co.uk>
2021-01-13 20:23:49 +0000

    Changed debug to true

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'm^7w3835%6rlu2#@cw=0t228fl%geai4l$+b-@z6jf(qo7pg=4'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['https://jld94-django-todo-app.herokuapp.com/']
 
